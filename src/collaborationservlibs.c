deda53c909ef9466e35e664b5035662dc99f3da7
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2013-05-07 15:20:42 +0000

    [OPENBUS-2247] Exportar luaopen_lsqlite3 [OPENBUS-2246] Criar puts/project-config.jam [OPENBUS-2233] Criar um descritor Puts + lsqlite3 como dependência no collaboration service. + Uso de luaopen_lsqlite3 em collaborationservlibs.c.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/services/collaboration/trunk@140590 ae0415b3-e90b-0410-900d-d0be9363c56b

--- src/collaborationservlibs.c
+++ src/collaborationservlibs.c
@@ -18,6 +18,7 @@
 #include "oil.h"
 #include "luascs.h"
 #include "luaopenbus.h"
+#include "lsqlite3.h"
 #include <collaborationService.h>
 
 char const* OPENBUS_MAIN = "openbus.services.collaboration.main";
@@ -32,6 +33,7 @@ void luapreload_extralibraries(lua_State *L)
 	lua_pushcfunction(L,luaopen_vararg);lua_setfield(L,-2,"vararg");
 	lua_pushcfunction(L,luaopen_struct);lua_setfield(L,-2,"struct");
 	lua_pushcfunction(L,luaopen_socket_core);lua_setfield(L,-2,"socket.core");
+	lua_pushcfunction(L,luaopen_lsqlite3);lua_setfield(L,-2,"lsqlite3");
 	lua_pop(L, 1);  /* pop 'package.preload' table */
 	/* preload other C libraries */
 	luapreload_lce(L);
